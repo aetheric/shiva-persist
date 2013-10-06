271cd107ed2b4a18b55cf4a0f6d917cce3adde5c
tzrlk <peterc@aetheric.co.nz>
2013-10-06 13:55:36 +1300

    Converted the IsEnabledFilter into an interface, because multiple inheritance in java isn't a thing.

--- src/main/java/nz/co/aetheric/shiva/persist/api/filters/IsEnabledFilter.java
+++ src/main/java/nz/co/aetheric/shiva/persist/api/filters/IsEnabledFilter.java
@@ -9,39 +9,27 @@ import nz.co.aetheric.shiva.persist.api.types.Enableable;
  * Concrete implementations of this class will determine how that is checked and applied.
  * <p>Author: <a href="http://gplus.to/tzrlk">Peter Cummuskey</a></p>
  */
-public abstract class IsEnabledFilter<QueryType extends Query<? extends Enableable, ?>>
-		implements QueryFilter<QueryType> {
-
-	protected final Boolean enabled;
-
-	/**
-	 * Instantiate the filter with the provided default filter value.
-	 * @param enabled Whether all of the desired results are enabled, disabled, or undetermined.
-	 */
-	protected IsEnabledFilter(Boolean enabled) {
-		this.enabled = enabled;
-	}
+public interface IsEnabledFilter<QueryType extends Query<? extends Enableable, ?>>
+		extends QueryFilter<QueryType> {
 
 	/**
 	 * Provides access to the default 'enabled' state of the filter.
 	 * @return Returns what 'enabled' state the filter is in.
 	 */
-	public Boolean getEnabled() {
-		return enabled;
-	}
+	public Boolean getEnabled();
 
 	/**
 	 * Will filter the provided query to ensure that all results match the default enabled state.
 	 * @param query The query to apply the filter to.
 	 */
 	@Override
-	public abstract void apply(QueryType query);
+	public void apply(QueryType query);
 
 	/**
 	 * This overloading of the {@link #apply(Query)} method allows the enabled state to be specified.
 	 * @param query The query to apply the filter to.
-	 * @param enabled Whether the
+	 * @param enabled Whether the entities are enabled, disabled or undetermined.
 	 */
-	public abstract void apply(QueryType query, Boolean enabled);
+	public void apply(QueryType query, Boolean enabled);
 
 }
